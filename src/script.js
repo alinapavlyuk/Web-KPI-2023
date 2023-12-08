7742dac60f315d00182b9eb7295ed147bb3daf98
alinapavlyuk <alino4kap12@gmail.com>
2023-12-08 23:57:18 +0900

    Made tabs clickable

--- src/script.js
+++ src/script.js
@@ -1,23 +1,34 @@
+function changeTabs(event, whiskSize) {
+  let deactivatedTabElems = document.querySelectorAll(".tab-current");
+  deactivatedTabElems.forEach((element) => {
+    element.classList.remove("tab-current");
+  });
+
+  document.getElementById(`${whiskSize}-whisk`).classList.add("tab-current");
+  document
+    .getElementById(`${whiskSize}-whisk-image`)
+    .classList.add("tab-current");
+  document
+    .getElementById(`${whiskSize}-whisk-price`)
+    .classList.add("tab-current");
+
+  event.currentTarget.classList.add("tab-current");
+}
+
+function hamburgerToggle() {
+  if (window.matchMedia("(max-width: 767px)").matches) {
+    navElement.style.display = hamburgerCheckbox.checked
+      ? "inline-block"
+      : "none";
+    headerElement.style.height = hamburgerCheckbox.checked ? "unset" : "70px";
+  }
+}
+
 const hamburgerCheckbox = document.getElementById("hamburger");
 const navListElements = document.querySelectorAll("#nav-list li a");
 
 const navElement = document.querySelector(".main-nav");
 const headerElement = document.querySelector("header");
-
-function setNavDisplay() {
-  if (window.matchMedia("(max-width: 767px)").matches) {
-    navElement.style.display = hamburgerCheckbox.checked ? "block" : "none";
-  } else {
-    navElement.style.display = "block";
-  }
-}
-
-function hamburgerToggle() {
-  if (window.matchMedia("(max-width: 767px)").matches) {
-    navElement.style.display = hamburgerCheckbox.checked ? "block" : "none";
-    headerElement.style.height = hamburgerCheckbox.checked ? "unset" : "70px";
-  }
-}
 
 hamburgerCheckbox.addEventListener("change", function () {
   hamburgerToggle();
@@ -31,9 +42,3 @@ navListElements.forEach((element) => {
     }
   });
 });
-
-window.addEventListener("resize", () => {
-  setNavDisplay();
-});
-
-setNavDisplay();
